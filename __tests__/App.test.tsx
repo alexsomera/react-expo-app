c3d9a3fcb90ee5257907049ddddf0626ceac4985
Alex Somera <45790723+alexsomera@users.noreply.github.com>
2025-08-17 11:08:47 -0300

    Create App.test.tsx

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../app/app';
+
+// Mocks necessários para o teste do componente Index
+jest.mock('expo-router', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+  })),
+  useLocalSearchParams: jest.fn(() => ({})),
+}));
+
+jest.mock('expo-font', () => ({
+  useFonts: jest.fn(() => [true, null]),
+  loadAsync: jest.fn(),
+}));
+
+jest.mock('@expo/vector-icons', () => ({
+  MaterialIcons: 'MaterialIcons',
+}));
+
+jest.mock('expo-constants', () => ({
+  manifest: {
+    extra: {
+      eas: {
+        projectId: 'your-project-id',
+      },
+    },
+  },
+}));
+
+jest.mock('../components/SearchBar', () => 'SearchBar');
+
+jest.mock('../ViewModel/UsersViewModel', () => ({
+  useUsersViewModel: jest.fn(() => ({
+    filteredUsers: [],
+    searchQuery: '',
+    setSearchQuery: jest.fn(),
+  })),
+}));
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Index />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
