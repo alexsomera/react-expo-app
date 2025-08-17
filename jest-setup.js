7211a8b35461347d914425b30cfc326c8ff7bf75
Alex Somera <45790723+alexsomera@users.noreply.github.com>
2025-08-17 11:07:36 -0300

    Create jest-setup.js

---
+++ jest-setup.js
@@ -0,0 +1,39 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+jest.mock('expo-router', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+  })),
+  useLocalSearchParams: jest.fn(() => ({})),
+}));
+
+jest.mock('expo-font', () => ({
+  useFonts: jest.fn(() => [true, null]),
+  loadAsync: jest.fn(),
+}));
+
+jest.mock('@expo/vector-icons', () => ({
+  MaterialIcons: 'MaterialIcons',
+}));
+
+jest.mock('expo-constants', () => ({
+  manifest: {
+    extra: {
+      eas: {
+        projectId: 'your-project-id',
+      },
+    },
+  },
+}));
+
+jest.mock('../components/SearchBar', () => 'SearchBar');
+
+jest.mock('../ViewModel/UsersViewModel', () => ({
+  useUsersViewModel: jest.fn(() => ({
+    filteredUsers: [],
+    searchQuery: '',
+    setSearchQuery: jest.fn(),
+  })),
+}));
